e115644a737bddc8a78a151c951da85b031e565f
maadhukiran <madhug13390@gmail.com>
2022-05-02 18:49:07 +0530

    update

---
+++ arg.sh
@@ -0,0 +1,8 @@
+echo "Arguments total: $#"
+echo "script name: $0"
+echo "scripts 1st Argument : $1"
+echo "scripts 2nd Argument : $2"
+echo "scripts 3rd Argument : $3"
+echo "List of all arguments: * $*"
+echo "List of all Arguments: @ $@"
+echo "PID of shell:$$"
